e45472d6c3c01d3fad1245014f1589004f1e3c9a
Joseph Loomis <73568630+Joehl1103@users.noreply.github.com>
2026-08-21 20:45:46 -0500

    added conditional render for no live demo

--- frontend/src/projects/project.js
+++ frontend/src/projects/project.js
@@ -11,19 +11,21 @@ class Project extends HTMLElement {
     this.title = this.dataset.title;
     this.description = this.dataset.description;
     this.stack = this.dataset.stack;
-    this.codeUrl = this.dataset.codeUrl;
-    this.demoUrl = this.dataset.demoUrl;
-    console.log("title", this.title);
+    this.codeUrl = this.dataset.codeurl;
+    console.log("this.codeUrl", this.codeUrl);
+    this.demoUrl = this.dataset.demourl;
+    console.log("demoUrl", this.demoUrl);
+    const demoExists = /^http/.test(this.demoUrl);
     this.innerHTML = `
    <li>
       <b>${this.title}</b>, ${this.description}</br> <i>
       ${this.stack}</i></br>
-      <a href=\"${this.codeUrl}\">Code</a
-      >
-      |
-      <a href=\"${this.demoUrl}\"
-        >Live Demo</a
-      >
+      <a href="${this.codeUrl}">Code</a> |
+      ${
+        demoExists
+          ? `<a href="${this.demoUrl}">Live Demo</a>`
+          : `${this.demoUrl}`
+      }
     </li>
   `;
   }
